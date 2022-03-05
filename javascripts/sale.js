6d463207300d18abee1edd14c970f317303a4a26
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-03-05 11:14:42 +0900

    Move: モジュール化する為、sale.js → util.js にコードを移動

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -51,28 +51,6 @@ function add() {
   numberElement.value = "";
 }
 
-function subtotal() {
-  return purchases.reduce((prev, purchase) => {
-    return prev + purchase.product.price * purchase.number;
-  }, 0)
-}
-
-function display() {
-  return purchases.map(purchase => {
-    return `${purchase.product.price}円:${purchase.number}点`;
-  }).join("\n")
-};
-
-function calcPostageFromPurchase(sum) {
-   if (sum == 0 || sum >= 3000) {
-     return 0;
-   } else if (sum < 1000){
-    return 500;
-   } else {
-    return 250;
-   }
-}
-
 function calc() {
   const sum = subtotal(purchases);
   const postage = calcPostageFromPurchase(sum);
